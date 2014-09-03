ca09dd42508cb17487ce07cf0243e3b8725ba8c1
Alexey Karazhey <grimreio@gmail.com>
2014-09-03 17:46:52 +0300

    module.exports

--- www/openiab.js
+++ www/openiab.js
@@ -7,19 +7,7 @@ function log(msg)
 	console.log("OpenIAB-js: " + msg);
 }
 
-exports.init = function(success, error) 
+module.exports.openiab.init = function(success, error)
 {
     exec(success, error, PLUGIN, "init", ["INIT-PARAM"]);
 }
-
-// openiab.init = function(callback)
-// {
-//     cordova.exec(
-//     	callback, 
-//     	function(error) 
-//     	{ 
-//     		log(error);
-//     		callback(error); 
-//     	}, 
-//     	PLUGIN, "init", ["INIT-PARAM"]);
-// }
