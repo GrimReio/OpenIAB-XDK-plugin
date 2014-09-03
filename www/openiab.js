4be9f631ba04b9ca99f0be021a44a3855260b1bc
Alexey Karazhey <grimreio@gmail.com>
2014-09-03 17:12:50 +0300

    plugin cfg update

--- www/openiab.js
+++ www/openiab.js
@@ -1,18 +1,25 @@
+var exec = require('cordova/exec');
+
+PLUGIN = "OpenIabCordovaPlugin";
+
 function log(msg)
 {
 	console.log("OpenIAB-js: " + msg);
 }
 
-PLUGIN = "OpenIabCordovaPlugin";
-
-openiab.init = function(callback)
+exports.init = function(success, error) 
 {
-    cordova.exec(
-    	callback, 
-    	function(error) 
-    	{ 
-    		log(error);
-    		callback(error); 
-    	}, 
-    	PLUGIN, "init", ["INIT-PARAM"]);
+    exec(success, error, PLUGIN, "init", ["INIT-PARAM"]);
 }
+
+// openiab.init = function(callback)
+// {
+//     cordova.exec(
+//     	callback, 
+//     	function(error) 
+//     	{ 
+//     		log(error);
+//     		callback(error); 
+//     	}, 
+//     	PLUGIN, "init", ["INIT-PARAM"]);
+// }
